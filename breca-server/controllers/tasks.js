8004cc6db700a59368266977e494bf45027e630e
Marco <a20111265@pucp.pe>
2018-06-26 19:16:17 -0500

    ident

--- breca-server/controllers/tasks.js
+++ breca-server/controllers/tasks.js
@@ -3,21 +3,21 @@ const _ = require('lodash');
 
 const tasksCtrl = {
   getCategories: function() {
-  	return _(data).map('DE_CATE').uniq().value();
-	},
-	getBrands: function(category) {
-  	return _(data).filter({DE_CATE:category}).map('DE_EQUI').uniq().value();
-	},
-	getFamilies: function(brand, category) {
-  	return _(data).filter({DE_EQUI:brand, DE_CATE:category}).map('DE_FAMI').uniq().value();
-	},
-	getProducts: function(category, brand, family) {
-		return _(data).filter({
-  			DE_CATE: category,
-  			DE_EQUI: brand,
-  			DE_FAMI: family
-  		});
-	}
+    return _(data).map('DE_CATE').uniq().value();
+  },
+  getBrands: function(category) {
+    return _(data).filter({DE_CATE:category}).map('DE_EQUI').uniq().value();
+  },
+  getFamilies: function(brand, category) {
+    return _(data).filter({DE_EQUI:brand, DE_CATE:category}).map('DE_FAMI').uniq().value();
+  },
+  getProducts: function(category, brand, family) {
+    return _(data).filter({
+        DE_CATE: category,
+        DE_EQUI: brand,
+        DE_FAMI: family
+      });
+  }
 }
 
 module.exports = tasksCtrl;
